34adcd4df4a1e7cd654c09276e43e5829b20f6ce
Li Yan <lantimilan@gmail.com>
2025-10-27 22:50:47 -0700

    itoa implementation

--- src/endianess.cc
+++ src/endianess.cc
@@ -1,6 +1,7 @@
 // endianess.cc
 #include <cstdio>
 #include <cstdint>
+#include <string>
 
 void fct(/*code*/const char* *pstr) {
   *pstr = "hello world";
@@ -8,14 +9,33 @@ void fct(/*code*/const char* *pstr) {
 
 bool bigendian()
 {
-  int32_t val = 1;
+  int32_t val = 0x11223344;
   char* p = reinterpret_cast<char*>(&val);
-  // for big endian, high byte appear first, so *p == 0
-  // for little endian, low byte appear first, so *p == 1
-  if (*p) printf("little endian\n");
+  // for big endian, high byte appear first, so *p == 0x11
+  // for little endian, low byte appear first, so *p == 0x44
+  if (*p == 0x44) printf("little endian\n");
   else printf("big endian\n");
 
-  return *p == 0;
+  return *p == 0x11;
+}
+
+std::string itoa(int32_t num) {
+  int64_t l = num;
+  bool neg = false;
+  if (l < 0) { l = -l; neg = true; }
+  std::string res;
+  do {
+    res += l % 10 + '0';
+    l /= 10;
+  } while (l);
+  if (neg) res += '-';
+  // reverse
+  for (int a = 0, b = res.length()-1; a < b; ++a, --b) {
+    char ch = res[a];
+    res[a] = res[b];
+    res[b] = ch;
+  }
+  return res;
 }
 
 int main()
@@ -25,4 +45,17 @@ int main()
   printf("%s\n", str);
 
   bigendian();
+
+  {
+    int32_t i = 123;
+    printf("itoa(%d) = %s\n", i, itoa(i).c_str());
+  }
+  {
+    int32_t i = 0;
+    printf("itoa(%d) = %s\n", i, itoa(i).c_str());
+  }
+  {
+    int32_t i = -123;
+    printf("itoa(%d) = %s\n", i, itoa(i).c_str());
+  }
 }
